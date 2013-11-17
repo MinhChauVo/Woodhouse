a6cfa6cb6ad062dd05bc959aaa95d6ea6d8cd702
Rodolfo Ovalles <rovalles@fossil.com>
2013-11-17 14:52:44 -0800

    commiting all that shit

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,5 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(-34.397, 150.644),
