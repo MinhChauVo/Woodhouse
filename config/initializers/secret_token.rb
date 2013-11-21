7d6e0924ae59b96ff5a4ccb8089f842f2eb74199
Rodolfo Ovalles <rovalles@fossil.com>
2013-11-20 21:58:08 -0800

    leave me alone ruby on rails

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Woodhouse::Application.config.secret_token = '852661547841165dda6772f30277a847ad5f38c79fc57c0803208e03333473ffaa480f6fb673eb17eaf167588cc9e7566fa598fc6655de121a37e712c482a39d'
