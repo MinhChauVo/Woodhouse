181e33316adcb139b3d4653057043709a0584833
Rodolfo Ovalles <rovalles@fossil.com>
2013-12-25 16:07:49 -0600

    djskdhkjsjkksd

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@
  */
 
 exports.index = function(req, res, db){
-    collection = db.collection('woodhouse');
-    collection.find().toArray(function(err, results) {
-        console.dir(results);
-        // Let's close the db
-    });
+    // collection = db.collection('woodhouse');
+    // collection.find().toArray(function(err, results) {
+    //     console.dir(results);
+    //     // Let's close the db
+    // });
     res.render('index', { title: 'Woodhouse' });
 };
 
