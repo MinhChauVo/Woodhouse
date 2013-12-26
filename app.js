7fd6676f4d3022ea70a7059176f480fb4e2d8191
Rodolfo Ovalles <rovalles@fossil.com>
2013-12-25 22:35:45 -0600

    Mongo DB call passed to route

--- app.js
+++ app.js
@@ -42,23 +42,6 @@ var generate_mongo_url = function(obj){
 
 var mongourl = generate_mongo_url(dbData);
 
-// var record_visit = function(req, res){
-//     /* Connect to the DB and auth */
-//     require('mongodb').connect(mongourl, function(err, conn){
-//         conn.collection('ips', function(err, coll){
-//             /* Simple object to insert: ip address and date */
-//             object_to_insert = { 'ip': req.connection.remoteAddress, 'ts': new Date() };
-//             /* Insert the object then print in response */
-//             /* Note the _id has been created */
-//             coll.insert( object_to_insert, {safe:true}, function(err){
-//                 res.writeHead(200, {'Content-Type': 'text/plain'});
-//                 res.write(JSON.stringify(object_to_insert));
-//                 res.end('\n');
-//             });
-//         });
-//     });
-// };
-
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -78,13 +61,14 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+var DB;
+MongoClient.connect(mongourl, function(err, db){
+    if(err) throw err;
+    DB = db;
+});
 
 app.get('/', function(req, res){
-    MongoClient.connect(mongourl, function(err, db){
-    //     if(err) throw err;
-    // routes.index(req, res, db);
-        routes.index(req, res);
-    });
+    routes.index(req, res, DB);
 });
 //app.get('/users', user.list);
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
-
 /*
  * GET home page.
  */
-
-exports.index = function(req, res, db){
-    // collection = db.collection('woodhouse');
-    // collection.find().toArray(function(err, results) {
-    //     console.dir(results);
+exports.index = function(req, res, DB){
+    collection = DB.collection('woodhouse');
+    collection.find().toArray(function(err, results) {
+        console.dir(results);
     //     // Let's close the db
-    // });
+    });
     res.render('index', { title: 'Woodhouse' });
 };
 
