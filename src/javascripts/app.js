6ca239b648b8cbbf2e34a1053188416507c93eec
Rodolfo Ovalles <rovalles@fossil.com>
2014-04-27 18:42:21 -0500

    shit just got real. pass locations to my dawgs

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -8,11 +8,6 @@
     // Emit ready event with room name.
     io.emit('ready', room);
 
-    // Listen for the announce event.
-    io.on('announce', function(data) {
-        console.log(data.message+ new Date().toString());
-    });
-
     woodhouse.controller('LunchApp', ['$scope', 'geolocation', 'gmap', '$timeout', function($scope, geolocation, gmap, $timeout) {
         $scope.map = {
             center: {
@@ -20,9 +15,9 @@
                 lng: 0
             },
             markers: [
-                { 'name': '635 & Toll Way', 'lat': 32.926193, 'lng': -96.822243, icon: 'friend' },
-                { 'name': 'Micking Bird', 'lat': 32.837806, 'lng': -96.774666, icon: 'friend' },
-                { 'name': 'Holy Grail Pub', 'lat': 32.78014, 'lng': -96.800451, icon: 'friend' }
+                //{ 'name': '635 & Toll Way', 'lat': 32.926193, 'lng': -96.822243, icon: 'friend' },
+                //{ 'name': 'Micking Bird', 'lat': 32.837806, 'lng': -96.774666, icon: 'friend' },
+                //{ 'name': 'Holy Grail Pub', 'lat': 32.78014, 'lng': -96.800451, icon: 'friend' }
             ],
             placesRadius: null
         };
@@ -43,7 +38,15 @@
         };
 
         geolocation.getCurrentLatLng().then(function(currentLocation) {
+            // Listen for the announce event.
+            io.on('announce', function(data) {
+                console.log(data.message+ new Date().toString(), currentLocation);
+                $scope.map.markers.push(currentLocation);
+                console.log($scope.map.markers, $scope.map.markers.length);
+            });
+
             currentLocation.icon = 'currentUser';
+            // $scope.map.markers.push(currentLocation);
             $scope.map.markers.push(currentLocation);
         });
     }]);
