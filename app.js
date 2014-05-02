0b1f830b57de9ef1af17a6ff32a090fd0f4ca4d3
Rodolfo Ovalles <rovalles@fossil.com>
2014-05-01 21:46:17 -0500

    Heroku setup

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var http = require('http');
 var path = require('path');
 var app = express();
 var routes = require('./routes');
+var port = process.env.PORT || 3000;
 app.http().io();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -38,5 +39,5 @@ app.get('/', routes.index);
 // http.createServer(app).listen(app.get('port'), function test () {
 //     console.log('Express server listening on port ' + app.get('port'));
 // });
-app.listen(3000);
+app.listen(port);
 console.log('Express is up and running on port 3000');
